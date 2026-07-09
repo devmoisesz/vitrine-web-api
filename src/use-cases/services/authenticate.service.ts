f483090457e17e9938ead344df9dfea7b8f5bc09
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-07-09 13:17:44 -0300

    feat: service authenticate

---
+++ src/use-cases/services/authenticate.service.ts
@@ -0,0 +1,26 @@
+import { BadRequestException, ConflictException, Injectable } from "@nestjs/common";
+import { UsersRepository } from "../../database/repositories/users-repository";
+import { InputAuthenticateDto } from "../dtos/authenticate.dto";
+import { compare } from "bcryptjs";
+import { User } from "@prisma/client";
+
+@Injectable()
+export class AuthenticateService {
+    constructor(private usersRepositoy: UsersRepository){}
+
+    async execute(data: InputAuthenticateDto): Promise<User> {
+        const user = await this.usersRepositoy.findByEmail(data.email)
+        
+        if(!user){
+            throw new BadRequestException('Invalid credentials')
+        }
+
+        const doesPasswordMatches = await compare(data.password, user.password!)
+
+        if(!doesPasswordMatches) {
+            throw new BadRequestException('Invalid credentials')
+        }
+
+        return user
+    }
+}
