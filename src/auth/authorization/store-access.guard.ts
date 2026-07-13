b77d8beb39a1889896bed04e7f41047e01c653b8
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-07-13 15:54:39 -0300

    fix: to also capture the slug from the parameter

--- src/auth/authorization/store-access.guard.ts
+++ src/auth/authorization/store-access.guard.ts
@@ -1,5 +1,13 @@
 import { PrismaService } from '@/database/prisma/prisma.service';
-import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { CollaboratorRole } from '@prisma/client';
 import { ROLES_KEY } from './roles.decorator';
@@ -12,58 +20,55 @@ export class StoreAccessGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-      const request = context.switchToHttp().getRequest()
-      const user = request.user
-      const storeId = request.params.storeId
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    
+    const { storeId, slug } = request.params;
 
-      if(!user){
-        throw new UnauthorizedException('Unauthorized.')
-      }
+    if (!user) {
+      throw new UnauthorizedException('Unauthorized.');
+    }
 
-      if(!storeId) {
-        throw new BadRequestException('Invalid request.')
-      } 
+    if (!storeId && !slug) {
+      throw new BadRequestException('Invalid request.');
+    }
 
-      const thisStoreExists = await this.prisma.store.findUnique({
-        where: {
-          id: storeId
-        }
-      })
+    const thisStoreExists = await this.prisma.store.findUnique({
+      where: storeId ? { id: storeId } : { slug: slug },
+    });
 
-      if(!thisStoreExists){
-        throw new NotFoundException('Resource not found.')
-      }
+    if (!thisStoreExists) {
+      throw new NotFoundException('Resource not found.');
+    }
 
-      if(user.role === 'Admin') {
-        return true;
-      } 
+    if (user.role === 'Admin') {
+      return true;
+    }
 
-      const collaborator = await this.prisma.collaborator.findFirst({
-        where: {
-            userId: user.id,
-            storeId: storeId
-        }
-      })
+    const collaborator = await this.prisma.collaborator.findFirst({
+      where: {
+        userId: user.id,
+        storeId: thisStoreExists.id,
+      },
+    });
 
-      if (!collaborator){
-        throw new ForbiddenException('You do not have permission to perform this action.')
-      }
+    if (!collaborator) {
+      throw new ForbiddenException('You do not have permission to perform this action.');
+    }
 
-      //Lê quais papéis a rota exige
-      const requiredRoles = this.reflector.getAllAndOverride<CollaboratorRole[]>(ROLES_KEY, [
-        context.getHandler(),
-        context.getClass()
-      ])
+    const requiredRoles = this.reflector.getAllAndOverride<CollaboratorRole[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
-      //Se a rota não exige um papel específico, libera o acesso caso pertence à loja)
-      if(!requiredRoles || requiredRoles.length === 0){
-        return true
-      }
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
 
-      if(!requiredRoles.includes(collaborator.role)){
-        throw new ForbiddenException('You do not have permission to perform this action.')
-      }
+    if (!requiredRoles.includes(collaborator.role)) {
+      throw new ForbiddenException('You do not have permission to perform this action.');
+    }
 
-      return true
+    return true;
   }
 }
