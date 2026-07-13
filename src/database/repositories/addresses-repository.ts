a70671f6a0b147bcd6fd39bed91ba8634abe101f
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-07-13 17:16:15 -0300

    feat: add new methods for the user and address repositories

--- src/database/repositories/addresses-repository.ts
+++ src/database/repositories/addresses-repository.ts
@@ -2,6 +2,7 @@ import { Address, Prisma } from "@prisma/client"
 
 export abstract class AddressRepository {
     abstract create(data: Prisma.AddressUncheckedCreateInput): Promise<Address>
+    abstract save(address: Address): Promise<Address>
     abstract findById(id: string): Promise<Address | null>
     abstract findByUserId(userId: string): Promise<Address[]>
     abstract findByStoreId(storeId: string): Promise<Address | null>
