c3e88117008525caa1dbfd61bd62a62f929f45c1
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-07-14 11:59:47 -0300

    feat: add service edit data store

---
+++ src/use-cases/services/stores/edit-data-store.service.ts
@@ -0,0 +1,67 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  InputEditDataStoreDto,
+  OutputEditDataStoreDto,
+} from './dtos/edit-data-store.dto';
+import { StoresRepository } from '@/database/repositories/stores-repository';
+import { SlugGeneratorService } from './utils/generate-slug.service';
+
+@Injectable()
+export class EditStoreDataService {
+  constructor(
+    private storesRepository: StoresRepository,
+    private slugGenerator: SlugGeneratorService,
+  ) {}
+
+  async execute(
+    slug: string,
+    data: InputEditDataStoreDto,
+  ): Promise<OutputEditDataStoreDto> {
+    const store = await this.storesRepository.findBySlug(slug);
+
+    if (!store) {
+      throw new NotFoundException('Store Not Found');
+    }
+
+    let updatedSlug = store.slug;
+    if (data.newName && data.newName != store.name) {
+      updatedSlug = await this.slugGenerator.execute(data.newName, store.id);
+    }
+
+    if (data.newEmail && data.newEmail != store.email) {
+      const isEmailDuplicate = await this.storesRepository.findBySLugAndEmail(
+        slug,
+        data.newEmail,
+      );
+
+      if (isEmailDuplicate) {
+        throw new ConflictException('Email already exists');
+      }
+    }
+
+    const newStoreData = await this.storesRepository.save({
+      id: store.id,
+      name: data.newName ?? store.name,
+      email: data.newEmail ?? store.email,
+      slug: updatedSlug,
+      description: data.newDescription ?? store.description,
+      whatsapp: store.whatsapp,
+      cnpj: store.cnpj,
+      cpf: store.cpf,
+      status: store.status,
+      logo_image_url: store.logo_image_url,
+      storage_public_id: store.storage_public_id,
+    });
+
+    return {
+      name: newStoreData.name,
+      slug: newStoreData.slug,
+      email: newStoreData.email,
+      description: newStoreData.description,
+    };
+  }
+}
--- test/factories/make-store.ts
+++ test/factories/make-store.ts
@@ -1,6 +1,7 @@
 import { StoresInMemoryRepository } from "../in-memory-repository/stores-in-memory-repository"
 import { randomUUID } from "node:crypto"
 import { faker } from '@faker-js/faker'
+import { makeEmail } from "./make-email"
 
 export function makeStore(
     inMemoryStoresRepository: StoresInMemoryRepository,
@@ -10,6 +11,7 @@ export function makeStore(
         id: randomUUID(),
         name: faker.person.fullName(),
         slug: 'slug-test',
+        email: makeEmail(),
         description: faker.lorem.text(),
         whatsapp: faker.phone.imei(),
         ...override
