f6b288e10f5d40f400f1a20d393b1e28c55516ec
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-07-25 13:11:03 -0300

    feat: add rate-limiting and CORS configurations for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { ConfigModule } from "@nestjs/config";
 import { envSchema } from './env/env';
 import { HttpModule } from './http/http.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+          ttl: 60000,
+          limit: 20
+      }
+    ]),
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
@@ -14,5 +22,11 @@ import { AuthModule } from './auth/auth.module';
     HttpModule,
     AuthModule, 
   ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser())
-  await app.listen(process.env.PORT ?? 3333);
+  app.use(cookieParser());
+  app.use(helmet());
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
+  const port = process.env.PORT || 3333;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
