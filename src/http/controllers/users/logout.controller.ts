fb0a3953ac338c317a12ba64d6b3cf7330ebf89e
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-07-27 09:12:44 -0300

    feat: add logout route

---
+++ src/http/controllers/users/logout.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, HttpCode, Post, Res } from '@nestjs/common';
+import type { Response } from 'express';
+
+@Controller('/logout')
+export class LogoutController {
+  @Post()
+  @HttpCode(200)
+  async handle(@Res({ passthrough: true }) res: Response) {
+    res.cookie('refreshToken', '', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', 
+      sameSite: 'strict',
+      maxAge: 0, 
+      path: '/',
+    });
+
+    return { message: 'Logout realizado com sucesso' };
+  }
+}
