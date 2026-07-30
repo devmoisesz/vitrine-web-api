ce40fa99d8f2952e979db585ddcc7c717c4de37e
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-07-29 21:51:00 -0300

    fix: changing the sameSite cookie to none

--- src/http/controllers/users/google-authenticate.controller.ts
+++ src/http/controllers/users/google-authenticate.controller.ts
@@ -22,7 +22,7 @@ export class GoogleAuthenticateController {
     res.cookie('refreshToken', refresh_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 1000 * 60 * 60, 
       path: '/',
     });
